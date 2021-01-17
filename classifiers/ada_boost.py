161056276fde0ced62d473aed1f26170a4bc04e2
Tarek Hany <tata.hany37@gmail.com>
2021-01-17 18:45:53 +0200

    ada_boost.py and random_forest.py

--- classifiers/ada_boost.py
+++ classifiers/ada_boost.py
@@ -1,10 +1,13 @@
 from constants import *
 from classifiers.classifiers import Classifiers
-
+from sklearn.ensemble import AdaBoostClassifier
 
 class AdaBoost(Classifiers):
     def __ada_boost(self):
-        pass
+        abc = AdaBoostClassifier(n_estimators=100, learning_rate=1)
+        model = abc.fit(self.sample_train, self.label_train)
+        self.label_predicted = model.predict(self.sample_test)
+        self.calculate_results()
 
     def train(self):
-        pass
+        self.__ada_boost()
--- classifiers/random_forest.py
+++ classifiers/random_forest.py
@@ -1,10 +1,15 @@
+from sklearn.ensemble import RandomForestClassifier
+
 from constants import *
 from classifiers.classifiers import Classifiers
 
 
 class RandomForest(Classifiers):
     def __random_forest(self):
-        pass
+        clf = RandomForestClassifier(n_estimators=100)
+        clf.fit(self.sample_train, self.label_train)
+        self.label_predicted = clf.predict(self.sample_test)
+        self.calculate_results()
 
     def train(self):
-        pass
+        self.__random_forest()
